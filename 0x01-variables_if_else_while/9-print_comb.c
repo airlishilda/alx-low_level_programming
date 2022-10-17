f022758e127d102eba6b7de76607ea6b7de7b3c5
airlishilda <airlishilda@gmail.com>
2022-10-17 05:59:25 -0700

    nine print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n + '0');
-		if (n < 9)
+		putchar((num % 10) + '0');
+		if (n == 9)
 		putchar(',');
 		putchar(' ');
 	}
