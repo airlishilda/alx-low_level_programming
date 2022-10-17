36b75f7e33016cc007646483a0b66b5094c39218
airlishilda <airlishilda@gmail.com>
2022-10-17 05:46:33 -0700

    nine print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,13 @@ int main(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
-{
+	{
 		putchar(n + '0');
 		if (n < 9)
-{
-			putchar(',');
-			putchar(' ');
-		}
-	}		
+		putchar(',');
+		putchar(' ')
+	}
+
 	putchar('\n');
 	return (0);
 }
