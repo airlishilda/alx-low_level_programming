a9155b8a7ed8443106ab82034e187efeeb973d6a
airlishilda <airlishilda@gmail.com>
2022-10-19 08:43:27 -0700

    task ten 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+include "main.h"
+
+/**
+ * add - Add two integer to print.
+ * @num1: first integer to be added to print.
+ * @num2: second integer to be added to print.
+ *
+ * Return: the final resultion of the addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
